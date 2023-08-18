db77e93c897edfe1cbc755a8597f892894aceca6
Beliya470 <anneanziay78@gmail.com>
2023-08-18 12:26:13 +0300

    Completed Assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
-#!/usr/bin/env python3
+def print_fibonacci(n):
+    if n <= 0:
+        print([])
+        return
+    elif n == 1:
+        print([0])
+        return
+    elif n == 2:
+        print([0, 1])
+        return
 
-def print_fibonacci(length):
-    pass
+    fibonacci_sequence = [0, 1]
+    for i in range(2, n):
+        next_val = fibonacci_sequence[i-1] + fibonacci_sequence[i-2]
+        fibonacci_sequence.append(next_val)
+
+    print(fibonacci_sequence)
